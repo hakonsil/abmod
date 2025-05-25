3139a5c5ac734e70c4a748e88700eb20230baa25
hakonsil <hakonsilseth01@gmail.com>
2025-05-25 17:40:50 +0200

    fwrc

---
+++ delete_after.py
@@ -0,0 +1,9 @@
+# copy the file /nfs/revontuli/data/juha/maarsy/2020/03/20200319_220733182_event.ud3.h5best_fit.png to this directory
+
+import os
+import glob
+import shutil
+
+file = '/nfs/revontuli/data/juha/maarsy/2020/03/20200319_220733182_event.ud3.h5best_fit.png'
+shutil.copy(file, '/home/hakon/Documents/abmod/20200319_220733182_event.ud3.h5best_fit.png')
+
